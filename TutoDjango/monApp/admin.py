7214b517164ffabc7176d9c58f011344590c63aa
OphelieVal <ophelie.valin@etu.univ-orleans.fr>
2025-09-11 10:35:58 +0200

    Optimisation filtrage prix et status

--- TutoDjango/monApp/admin.py
+++ TutoDjango/monApp/admin.py
@@ -1,3 +1,4 @@
+from decimal import ROUND_HALF_UP, Decimal
 from django.contrib import admin
 from .models import *
 
@@ -7,14 +8,21 @@ class ProduitFilter(admin.SimpleListFilter):
     
     def lookups(self, request, model_admin) :
         return (
-        ('OnLine', 'En ligne'),
-        ('OffLine', 'Hors ligne'),
+        ('Online', 'En ligne'),
+        ('Offline', 'Hors ligne'),
         )
     def queryset(self, request, queryset):
-        if self.value() == 'OnLine':
+        if self.value() == 'Online':
             return queryset.filter(status=1)
-        if self.value() == 'OffLine':
+        if self.value() == 'Offline':
             return queryset.filter(status=0)
+
+def set_Produit_online(modeladmin, request, queryset):
+    queryset.update(status=1)
+set_Produit_online.short_description = "Mettre en ligne"
+def set_Produit_offline(modeladmin, request, queryset):
+    queryset.update(status=0)
+set_Produit_offline.short_description = "Mettre hors ligne"
 
 class ProduitAdmin(admin.ModelAdmin):
     model = Produit
@@ -25,6 +33,13 @@ class ProduitAdmin(admin.ModelAdmin):
     list_filter = (ProduitFilter,)
     date_hierarchy = 'dateFabProd'
     ordering = ('-dateFabProd',)
+    actions = [set_Produit_online, set_Produit_offline]
+
+    def prixTTCProd(self, instance):
+        return (instance.prixUnitaireProd * Decimal('1.20')).quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
+    prixTTCProd.short_description = "Prix TTC"
+    prixTTCProd.short_description = "Prix TTC"
+    prixTTCProd.admin_order_field = "prixUnitaireProd"
 
 class ProduitInline(admin.TabularInline):
     model = Produit
