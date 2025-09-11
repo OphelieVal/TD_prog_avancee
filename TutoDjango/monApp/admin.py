3b3ed6d2d8631a6aacc3a38c491a0dbe3da1dcc4
OphelieVal <ophelie.valin@etu.univ-orleans.fr>
2025-09-11 10:23:35 +0200

    Affinage du système de filtre

--- TutoDjango/monApp/admin.py
+++ TutoDjango/monApp/admin.py
@@ -1,13 +1,30 @@
 from django.contrib import admin
 from .models import *
 
+class ProduitFilter(admin.SimpleListFilter):
+    title = 'filtre produit'
+    parameter_name = 'custom_status'
+    
+    def lookups(self, request, model_admin) :
+        return (
+        ('OnLine', 'En ligne'),
+        ('OffLine', 'Hors ligne'),
+        )
+    def queryset(self, request, queryset):
+        if self.value() == 'OnLine':
+            return queryset.filter(status=1)
+        if self.value() == 'OffLine':
+            return queryset.filter(status=0)
+
 class ProduitAdmin(admin.ModelAdmin):
     model = Produit
     list_display =  ["refProd", "intituleProd", "prixUnitaireProd", "dateFabProd", "categorie", "status"]
     list_editable = ["intituleProd", "prixUnitaireProd", "dateFabProd"]
     radio_fields = {"status": admin.VERTICAL}
     search_fields = ('intituleProd', 'dateFabProd')
-    list_filter = ('status', 'dateFabProd')
+    list_filter = (ProduitFilter,)
+    date_hierarchy = 'dateFabProd'
+    ordering = ('-dateFabProd',)
 
 class ProduitInline(admin.TabularInline):
     model = Produit
