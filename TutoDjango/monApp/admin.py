a1906bf880a9c60f018669a9ef59a6520032c28f
OphelieVal <ophelie.valin@etu.univ-orleans.fr>
2025-09-11 10:09:59 +0200

    Création de produits depuis une catégorie et affichage contenu

--- TutoDjango/monApp/admin.py
+++ TutoDjango/monApp/admin.py
@@ -4,8 +4,16 @@ from .models import *
 class ProduitAdmin(admin.ModelAdmin):
     list_display = ('intituleProd', 'prixUnitaireProd')
 
+class ProduitInline(admin.TabularInline):
+    model = Produit
+    extra = 1 # nombre de lignes vides par défaut
+
+class CategorieAdmin(admin.ModelAdmin):
+    model = Categorie
+    inlines = [ProduitInline]
+
 admin.site.register(Produit, ProduitAdmin)
-admin.site.register(Categorie)
+admin.site.register(Categorie, CategorieAdmin)
 admin.site.register(Statut)
 admin.site.register(Rayon)
 admin.site.register(Contenir)
